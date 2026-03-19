de5593ed7dbcfe7e81050bf19a8c665662e3631a
jakomycat <jchalarcav@gmail.com>
2026-03-18 21:33:17 -0500

    Functions added to calculate KL divergence and reciprocal rank.

--- src/notebooks_utils.py
+++ src/notebooks_utils.py
@@ -1,3 +1,6 @@
+import torch
+import torch.nn.functional as F
+
 # Get probability that the word
 def get_word_probability(layers_predictions, word):
     probs = []
@@ -10,3 +13,22 @@ def get_word_probability(layers_predictions, word):
         if len(probs) != i: probs.append(0.0) # If the word don't exist in the preds, prob = 0
         
     return probs
+
+#
+def calculate_kl_divergence(logits_layer, final_logits):
+    log_probs_lens = F.log_softmax(logits_layer, dim=-1) # kl_div requires this in log-probabilities
+    probs_final = F.softmax(final_logits, dim=-1)
+
+    kl_div = F.kl_div(log_probs_lens, probs_final, reduction='batchmean')
+
+    return kl_div
+
+def calculate_reciprocal_rank(layer_logits, target_token_id):
+    sorted_indices = torch.argsort(layer_logits, descending=True)
+    
+    rank_tensor = (sorted_indices == target_token_id).nonzero(as_tuple=True)[0]
+    rank_position = rank_tensor.item() + 1
+    
+    reciprocal_rank = 1.0 / rank_position
+    
+    return reciprocal_rank, rank_position
